ef12207e546341eb13649a0c757b06b320ade12f
KoikeTakayuki <tekasax@gmail.com>
2017-04-21 15:52:02 +0900

    Fix getRootType

---
+++ test/get-root-type-test.js
@@ -0,0 +1,21 @@
+const should = require('should');
+const getRootType = require('../util/get-root-type');
+const CoreTypes = require('ameba-core').Types;
+
+describe('getRootType', function () {
+  it('should return Record when Record given', function () {
+    getRootType(CoreTypes.Record).should.have.property('id', CoreTypes.Record.id);
+  });
+
+  it('should return Trait when Trait given', function () {
+    getRootType(CoreTypes.Trait).should.have.property('id', CoreTypes.Trait.id);
+  });
+
+  it('should return Trait when RecordType given', function () {
+    getRootType(CoreTypes.RecordType).should.have.property('id', CoreTypes.Trait.id);
+  });
+
+  it('should return Field when Field given', function () {
+    getRootType(CoreTypes.Field).should.have.property('id', CoreTypes.Field.id);
+  });
+});
--- util/get-root-type.js
+++ util/get-root-type.js
@@ -1,7 +1,7 @@
 const Record = require('ameba-core').Types.Record;
 
 function getRootType(recordType) {
-  if (recordType.baseType.id === Record) {
+  if (recordType.baseType.id === Record.id) {
     return recordType;
   }
 
