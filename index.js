dff78aa0910a6fb8dd003b3fe8643898cdc8e823
KoikeTakayuki <tekasax@gmail.com>
2017-04-21 12:48:56 +0900

    Misc fix

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const isBoolean = require('./util/is-boolean');
 const isNumber = require('./util/is-number');
 const isString = require('./util/is-string');
 const isObject = require('./util/is-object');
-const isList = require('./util/is-list');
 const isRecord = require('./util/is-record');
 const isRecordType = require('./util/is-record-type');
+const isInstanceOf = require('./util/is-instance-of');
 const getHierarchyFields = require('./util/get-hierarchy-fields');
 const createRecord = require('./util/create-record');
 const getRootType = require('./util/get-root-type');
@@ -14,9 +14,9 @@ module.exports = {
   isNumber,
   isString,
   isObject,
-  isList,
   isRecord,
   isRecordType,
+  isInstanceOf,
   getRootType,
   getHierarchyFields,
   createRecord,
